b0f2c4d614e71bfca305bb91392d5fd3a94e9fb2
Baarbod Ashenagar <baarbod.ashen@gmail.com>
2026-03-16 09:47:43 -0400

    updated velocty inference function, and added new area scaling

--- tofinv/utils.py
+++ tofinv/utils.py
@@ -15,7 +15,6 @@ def add_baseline_period(t, x, baseline_duration, baseline_value=0.0):
     t_with_baseline = np.linspace(0, t.max() + baseline_duration, np.size(x_with_baseline))
     return t_with_baseline, x_with_baseline
 
-
 def upsample(y_input, n, tr):
     if y_input.ndim == 1:
         y_input = np.expand_dims(y_input, 0).T
@@ -31,7 +30,6 @@ def upsample(y_input, n, tr):
 def input_batched_signal_into_NN_area(s_data_for_nn, NN_model, xarea, area):
     ntime = s_data_for_nn.shape[0]
     num_slice_to_use = s_data_for_nn.shape[1]
-    nfeature = num_slice_to_use + 2 # area takes up last two features
     feature_length = xarea.size
     nwindows = ntime // feature_length
     remainder = ntime % feature_length
@@ -39,16 +37,20 @@ def input_batched_signal_into_NN_area(s_data_for_nn, NN_model, xarea, area):
     velocity_NN = np.zeros((nwindows + (1 if remainder > 0 else 0)) * feature_length)
     
     def run_window(s_window, start_idx):
-        x_np = np.zeros((1, nfeature, feature_length))
+        flow_np = np.zeros((1, num_slice_to_use, feature_length))
         for islice in range(num_slice_to_use):
-            x_np[0, islice, :] = s_window[:, islice].squeeze()
-        x_np[0, -2, :] = xarea
-        x_np[0, -1, :] = area
+            flow_np[0, islice, :] = s_window[:, islice].squeeze()
+            
+        area_np = np.zeros((1, 1, feature_length))
+        area_np[0, 0, :] = area
         
         device = next(NN_model.parameters()).device
-        x = torch.from_numpy(x_np).float().to(device)
-        y_predicted_tensor = NN_model(x)
+        flow_x = torch.from_numpy(flow_np).float().to(device)
+        area_x = torch.from_numpy(area_np).float().to(device)
+        
+        y_predicted_tensor = NN_model(flow_x, area_x)
         y_predicted = y_predicted_tensor.detach().cpu().numpy().squeeze()
+        
         velocity_NN[start_idx:start_idx + feature_length] = y_predicted
 
     for w in range(nwindows):
@@ -69,3 +71,8 @@ def scale_data(s):
     raw_mean = np.mean(s, axis=0)
     detrended = signal.detrend(s, axis=0)
     return detrended / raw_mean
+
+def scale_area(xarea, area):
+    middle_index = xarea.size // 2
+    area_scaled = area / area[middle_index]
+    return area_scaled
